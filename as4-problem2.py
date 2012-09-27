977add3bbce1e53ae98682aa4c946b36e956aa96
Rafael Adel <rafael.adel20@gmail.com>
2012-09-27 03:10:28 +0200

    fixed function name

--- as4-problem2.py
+++ as4-problem2.py
@@ -1,4 +1,4 @@
-def nestEggFixed(salary, save, growth):	
+def nestEggVariable(salary, save, growth):	
 	retire_list = []
 	retire_list.append(salary*save)
 	for i in range(0,len(growth)):
@@ -6,4 +6,4 @@ def nestEggFixed(salary, save, growth):
 		print "Year : %d" % i, "Fund : %f" % retire_list[i], "of growth : %f" % growth[i]
 		
 g_arr = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9 ,1.0]
-nestEggFixed(500, 0.1, g_arr)
+nestEggVariable(500, 0.1, g_arr)
